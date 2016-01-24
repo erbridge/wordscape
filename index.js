eadfbd1e5eb4c06746ab4c91c2be96d2cb06354d
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-01-24 14:23:02 +0000

    Remove the online requirement

--- index.js
+++ index.js
@@ -4,11 +4,6 @@
 
 const electron = require('electron');
 
-if (!navigator.onLine) {
-  alert(
-    'You appear to be offline.\n\nThis app requires internet access to run.'
-  );
-}
 
 electron.ipcRenderer.on('clear', function clearWords() {
   document.body.innerHTML = '';
