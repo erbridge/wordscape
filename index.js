1425f5244724b1195362ad2acb40c10b2ba34b42
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2016-01-24 14:23:18 +0000

    Fade the text in to a canvas

--- index.js
+++ index.js
@@ -4,11 +4,46 @@
 
 const electron = require('electron');
 
+const BACKGROUND_COLOUR = 'rgba(255, 255, 255, 1)';
+
+const canvas  = document.createElement('canvas');
+const context = canvas.getContext('2d');
+
+document.body.appendChild(canvas);
+
+const drawText = function drawText(text, x, y, colour) {
+  context.fillStyle = colour;
+  context.font = 'normal 20pt Arial';
+  context.fillText(text, x, y);
+};
+
+const displayText = function displayText(text, x, y, alpha) {
+  drawText(text, x, y, `rgba(0, 0, 0, ${alpha})`);
+};
+
+const clearText = function clearText(text, x, y) {
+  drawText(text, x, y, BACKGROUND_COLOUR);
+};
+
+const fadeInText = function fadeInText(text, x, y) {
+  let alpha = 0;
+
+  const interval = setInterval(function fade() {
+    clearText(text, x, y);
+    displayText(text, x, y, alpha);
+
+    alpha += 0.05;
+
+    if (alpha > 1) {
+      clearInterval(interval);
+    }
+  }, 50);
+};
 
 electron.ipcRenderer.on('clear', function clearWords() {
-  document.body.innerHTML = '';
+  context.clearRect(0, 0, canvas.width, canvas.height);
 });
 
 electron.ipcRenderer.on('display-words', function displayWords(event, words) {
-  document.write(words.join('<br>'));
+  fadeInText(words.join('\n'), 0, canvas.height / 2);
 });
